20943bd71d640942fdbe908a017db44fadb9b3f4
Mohammed El Himar <mohamed.elhimar04@gmail.com>
2026-05-07 20:13:31 +0100

    initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,14 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navbar } from './navbar/navbar';
+
+@NgModule({
+  declarations: [App, Navbar],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
+})
+export class AppModule {}
