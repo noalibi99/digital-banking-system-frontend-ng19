c055a0daac117664438f4b6179d7ac6b5c44d298
Mohammed El Himar <mohamed.elhimar04@gmail.com>
2026-05-09 01:38:17 +0100

    remove old files

--- accounts/accounts.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-accounts',
-  standalone: false,
-  templateUrl: './accounts.component.html',
-  styleUrl: './accounts.component.css',
-})
-export class AccountsComponent {}
